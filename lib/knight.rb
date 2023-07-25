658ce024766f8ab70ec8ed82f6ad2cadfe7e5d05
Dylan Palmboom <nofxboy1234@gmail.com>
2023-07-25 18:19:06 +0200

    WIP

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,13 +1,51 @@
+require_relative 'graph/node'
+require_relative 'position'
+
 class Knight
   attr_reader :position, :moves
-  attr_accessor :routes
+  attr_accessor :stops
 
   def initialize(position)
     @moves = [[-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2], [2, -1], [2, 1]]
     @position = position
   end
 
-  def shortest_path_to_destination
-    routes.children.map { |node| node.data }
+  def shortest_path_to_destination(destination)
+    root_node = stops
+    destination_node = Node.new(Position.new(destination.first, destination.last))
+    routes = draw_routes(root_node, destination_node)
+
+    shortest_route = nil
+    routes.each do |route|
+      shortest_route = route if (route.length - 1) > (shortest_route.length - 1)
+    end
+    shortest_route
+  end
+
+  private
+
+  def draw_routes(root, destination, routes = [])
+    # byebug
+    # Add root node to routes array
+    routes << root
+    # if root.data == position
+    # else
+    #   routes << root
+    # end
+    # If the chosen child.data == destination, return routes array
+    return routes if root.data == destination.data
+
+    # <Iterate> through each child in children
+    root.children.each do |child_node|
+      # Route through next child (Recurse) - exclude past nodes in routes array .last
+      if routes.last.find { |node| node.data == child_node.data }
+        next
+      else
+        draw_routes(child_node, destination, routes)
+      end
+    end
+
+    # Return routes array
+    routes
   end
 end
--- lib/main.rb
+++ lib/main.rb
@@ -8,8 +8,8 @@ require_relative 'position'
 board = Board.new(3, 3)
 knight = Knight.new(Position.new(0, 0))
 graph = Graph.new(board, knight)
-knight.routes = graph.root
+knight.stops = graph.root
 # binding.pry
-p knight.shortest_path_to_destination
+p knight.shortest_path_to_destination([1, 2])
 
 
