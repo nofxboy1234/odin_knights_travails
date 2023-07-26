47c443c7c52faa60731e03722bc4da36670d7bcd
Dylan Palmboom <nofxboy1234@gmail.com>
2023-07-26 15:41:43 +0200

    Update route history recording

--- lib/knight.rb
+++ lib/knight.rb
@@ -41,27 +41,49 @@ class Knight
   # more than 1 path? - recursion
   # how to record all path options? - recursion
   # [[{1, 2}], [{2, 1}], [{0, 2}], [{1, 0}], [{2, 2}], [{0, 1}], [{2, 0}]]
+
   def draw_routes(root, destination, routes = [], route_piece = [])
-    byebug
+    # byebug
     
     route_piece.push(root) unless root.data == position
 
     if root == destination
-      return
+      return route_piece
     end
     
     root.children.each do |child_node|
       # rubocop:disable Style/IfUnlessModifier
-      # unless route_piece.include?(child_node)
       unless route_piece.include?(child_node) || child_node.data == position
-        draw_routes(child_node, destination, routes, route_piece)
-
+        routes << draw_routes(child_node, destination, routes, route_piece)
+        
         # Create new route pieces and add to routes when destination reached
-        routes << route_piece
         route_piece = []
       end
       # rubocop:enable Style/IfUnlessModifier
     end
     routes
   end
+
+  # def draw_routes(root, destination, routes = [], route_piece = [])
+  #   byebug
+    
+  #   route_piece.push(root) unless root.data == position
+
+  #   if root == destination
+  #     routes << route_piece
+  #     return
+  #   end
+    
+  #   root.children.each do |child_node|
+  #     # rubocop:disable Style/IfUnlessModifier
+  #     unless route_piece.include?(child_node) || child_node.data == position
+  #       draw_routes(child_node, destination, routes, route_piece)
+
+  #       # Create new route pieces and add to routes when destination reached
+  #       route_piece = []
+  #     end
+  #     # rubocop:enable Style/IfUnlessModifier
+  #   end
+  #   routes
+  # end
 end
