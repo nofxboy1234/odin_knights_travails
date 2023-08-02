f1c3d6d7c836998d55593d7b8ac6b3d98d2bdef9
Dylan Palmboom <nofxboy1234@gmail.com>
2023-08-02 10:46:13 +0200

    Update shortest route algo

--- lib/knight.rb
+++ lib/knight.rb
@@ -47,12 +47,12 @@ class Knight
 
   def route_to_parent_of_child(routes, root_node, child, parent = nil)
     parent = routes[root_node]
-    
+
     # base case
     if parent.children.include?(child)
       route = []
       route.unshift(parent)
-      
+
       node = parent
       until node == root_node
         route.unshift(node)
@@ -68,32 +68,32 @@ class Knight
   end
 
   def draw_routes_iterative(current, destination)
-    byebug
+    # byebug
 
     root_node = current
 
     route = []
-    routes = {}
-    routes[current] = {}
+    # routes = {}
+    # routes[current] = {}
 
     queue = []
     queue.push(current)
 
     until queue.empty?
-      if current.parent
-        unless current.children.include?(queue.first)
-          # route.pop until route.last == current.parent 
+      # if current.parent
+      #   unless current.children.include?(queue.first)
 
-          # go back to node that has queue.first as a child
-          until route.last.children.include?(queue.first)
-            route.pop
-          end
-        end
-      end
+      #     # go back to node that has queue.first as a child
+      #     until route.last.children.include?(queue.first)
+      #       route.pop
+      #     end
+      #   end
+      # end
 
       current = queue.shift
       route.push(current)
 
+      # --base case
       if current == destination
         puts 'destination reached!'
         # traverse back to root node and count 'moves' to get shortest route
@@ -102,39 +102,39 @@ class Knight
           shortest_route.unshift(current)
           current = current.parent
         end
+        shortest_route.unshift(root_node)
 
         return shortest_route
       end
-      
-      current_child_routes = routes.dig(*route)
-      unless current_child_routes
-        route_to_parent = route[0..-2]
-        routes.dig(*route_to_parent)[current] = {}
-      end
+
+      # --recursive case
+
+      # current_child_routes = routes.dig(*route)
+      # unless current_child_routes
+      #   route_to_parent = route[0..-2]
+      #   routes.dig(*route_to_parent)[current] = {}
+      # end
 
       current.children.each do |child_node|
-        next if node_is_start_node?(child_node) ||
-        node_visited?(child_node, routes, route)
-        
-        routes.dig(*route)[child_node] = {}
-        
+        next if child_node == root_node ||
+                route.include?(child_node)
+
+        # routes.dig(*route)[child_node] = {}
+
         child_node.parent = current
         queue.push(child_node)
       end
     end
-
-    # routes
-    # byebug
   end
 
-  def node_is_start_node?(child_node)
-    child_node.data == position
-  end
+  # def node_is_start_node?(child_node)
+  #   child_node.data == position
+  # end
 
-  def node_visited?(child_node, routes, route)
-    has_existing_child_route = routes.dig(*route).has_key?(child_node)
-    child_node_in_current_route = route.include?(child_node)
+  # def node_visited?(child_node, routes, route)
+  #   has_existing_child_route = routes.dig(*route).has_key?(child_node)
+  #   child_node_in_current_route = route.include?(child_node)
 
-    has_existing_child_route || child_node_in_current_route
-  end
+  #   has_existing_child_route || child_node_in_current_route
+  # end
 end
--- lib/main.rb
+++ lib/main.rb
@@ -11,6 +11,6 @@ graph = Graph.new(board, knight)
 knight.stops = graph.root
 
 shortest_route = knight.shortest_path_to_destination([1, 2])
-puts "The shortest route is #{shortest_route.unshift(knight.position)}"
+puts "The shortest route is #{shortest_route}"
 # p all_possible_routes.values.size
 
