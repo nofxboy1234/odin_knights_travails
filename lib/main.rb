563673c1e7df9dd35400cf6d6df8171ae9cae86a
Dylan Palmboom <nofxboy1234@gmail.com>
2023-08-02 12:49:06 +0200

    Update main driver script

--- lib/main.rb
+++ lib/main.rb
@@ -4,10 +4,14 @@ require_relative 'knight'
 require_relative 'board'
 require_relative 'position'
 
-board = Board.new(4, 4)
-knight = Knight.new(Position.new(0, 0))
+start_position = [0, 0]
+end_position = [1, 2]
+board_size = [4, 4]
+
+board = Board.new(*board_size)
+knight = Knight.new(Position.new(*start_position))
 graph = Graph.new(board, knight)
 knight.stops = graph.root
 
-shortest_route = knight.shortest_path_to_destination([1, 2])
-puts "The shortest route is #{shortest_route}"
+shortest_route = knight.shortest_path_to_destination(end_position)
+puts "The shortest route for a Knight from #{start_position} to #{end_position} on a #{board_size.first}x#{board_size.last} chess board is #{shortest_route}"
