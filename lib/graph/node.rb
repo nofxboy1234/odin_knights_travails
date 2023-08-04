a0b82de55d95521376eca2be3eb0f224bbd6e43f
Dylan Palmboom <nofxboy1234@gmail.com>
2023-08-04 11:09:10 +0200

    WIP

--- lib/graph/node.rb
+++ lib/graph/node.rb
@@ -15,11 +15,11 @@ class Node
     @data = data
   end
 
-  def valid?
-    x_valid = x_pos >= 0 && x_pos <= 7
-    y_valid = y_pos >= 0 && y_pos <= 7
-    x_valid && y_valid
-  end
+  # def valid?
+  #   x_valid = x_pos >= 0 && x_pos <= 7
+  #   y_valid = y_pos >= 0 && y_pos <= 7
+  #   x_valid && y_valid
+  # end
 
   def to_s
     "{#{data.x_pos}, #{data.y_pos}}"
--- lib/knight.rb
+++ lib/knight.rb
@@ -36,14 +36,35 @@ class Knight
       end
 
       # --recursive case
-      current.children.each do |child_node|
-        next if child_node == root_node ||
-                route(current, root_node).include?(child_node)
-
-        child_node.parent = current
-        queue.push(child_node)
-      end
+      closest = closest_child(current, destination, root_node)
+      closest.parent = current
+      queue.push(closest)
     end
+  end
+
+  def closest_child(parent, destination, root_node)
+    byebug
+    closest = nil
+    parent.children.each do |child_node|
+      next if child_node == root_node ||
+              route(parent, root_node).include?(child_node)
+
+      x_distance = (destination.data.x_pos -
+                    child_node.data.x_pos).abs
+
+      y_distance = (destination.data.y_pos -
+                    child_node.data.y_pos).abs
+
+      next if x_distance < 1 || y_distance < 2
+
+      closest = if closest
+                  [closest, child_node].min
+                else
+                  child_node
+                end
+    end
+
+    closest
   end
 
   def route(current_node, root_node)
